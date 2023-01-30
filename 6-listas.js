68e900c2df9131e60fc8f62a5c267d4930b85b92
Joao Lino <linusmannuel@gmail.com>
2023-01-30 19:34:30 +0100

    feat: Imprimindo lista de cidades

--- 6-listas.js
+++ 6-listas.js
@@ -2,3 +2,5 @@ console.log(`Trabalhando com listas`);
 const salvador = `Salvador`;
 const saoPaulo = `SãoPaulo`;
 const rioDeJaneiro = `Rio de Janeiro`;
+
+console.log(salvador, saoPaulo, rioDeJaneiro);
