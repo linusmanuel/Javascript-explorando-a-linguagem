c33765688b81721f0620de003507712449005f52
Joao Lino <linusmannuel@gmail.com>
2023-01-30 19:43:41 +0100

    refactor: Usando arrays

--- 6-listas.js
+++ 6-listas.js
@@ -1,6 +1,8 @@
 console.log(`Trabalhando com listas`);
-const salvador = `Salvador`;
-const saoPaulo = `SãoPaulo`;
-const rioDeJaneiro = `Rio de Janeiro`;
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
 
-console.log(salvador, saoPaulo, rioDeJaneiro);
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+console.log(listaDeDestinos);
