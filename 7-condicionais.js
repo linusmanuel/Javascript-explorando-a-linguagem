2c16545f8f73b7c93085e6bdc980ad6b954bf448
Joao Lino <linusmannuel@gmail.com>
2023-02-03 10:07:41 +0100

    feat: adicionando elses

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -4,7 +4,8 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
 listaDeDestinos.push('Luanda');
 
-const idadeComprador = 16;
+const idadeComprador = 12;
+const estaAcomphado = false;
 console.log('Destinos Possíveis: ');
 console.log(listaDeDestinos);
 
@@ -13,6 +14,10 @@ const maior = 18;
 if (idadeComprador >= maior) {
 	listaDeDestinos.splice(2, 1);
 } else {
+	if (estaAcomphado) {
+		console.log('Comprador esta acompanhado!');
+		listaDeDestinos.splice(2, 1);
+	}
 	console.log('Você não é maior de idade, não posso vender!');
 }
 
