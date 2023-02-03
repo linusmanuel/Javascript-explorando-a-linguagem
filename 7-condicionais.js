e1a1b59da54667ed9ce38c42c2e2786a88e810c7
Joao Lino <linusmannuel@gmail.com>
2023-02-03 10:25:40 +0100

    feat: adiciona operador ternary

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -4,18 +4,16 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
 listaDeDestinos.push('Luanda');
 
-const idadeComprador = 12;
-const estaAcomphado = false;
+const idadeComprador = 18;
+const estaAcomphado = true;
 console.log('Destinos Possíveis: ');
 console.log(listaDeDestinos);
 
-const maior = 15;
+const maiorIdade = idadeComprador >= 18;
 
-if (idadeComprador >= maior) {
+if (maiorIdade || estaAcomphado) {
 	listaDeDestinos.splice(2, 1);
-} else if (estaAcomphado) {
-	console.log('Comprador esta acompanhado!');
-	listaDeDestinos.splice(2, 1);
+	estaAcomphado ? console.log('Comprador está acompanhado!') : '';
 } else {
 	console.log('Você não é maior de idade, não posso vender!');
 }
